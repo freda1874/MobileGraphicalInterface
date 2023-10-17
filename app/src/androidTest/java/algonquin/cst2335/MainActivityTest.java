24f09224167885440d8a338226d38aa8e42283ca
Freda Luo <freda.luo@gmail.com>
2023-10-17 14:56:05 -0400

    Merged test classes into one main class
    Signed-off-by: Freda Luo <freda.luo@gmail.com>

--- app/src/androidTest/java/algonquin/cst2335/MainActivityTest.java
+++ app/src/androidTest/java/algonquin/cst2335/MainActivityTest.java
@@ -59,4 +59,122 @@ MainActivityTest {
         ViewInteraction textView = onView(withId(R.id.textView));
 //        check that its text matches "You shall not pass!"
         textView.check(matches(withText("You shall not pass!")));
-    }}
+    }
+    /**
+     * This test method inputs a password missing a digit  and checks
+     * if the correct error message "You shall not pass!" is displayed.
+     */
+    @Test
+    public void testFindMissingDigit() {
+
+
+        ViewInteraction appCompatEditText = onView(withId(R.id.editText));
+
+        appCompatEditText.perform(replaceText("Password#$"));
+
+//        find the button with id R.id.button.
+        ViewInteraction materialButton = onView(
+                withId(R.id.button));
+//        perform clicking that button
+        materialButton.perform(click());
+//        find the view with id R.id.textView
+        ViewInteraction textView = onView(withId(R.id.textView));
+//        check that its text matches "You shall not pass!"
+        textView.check(matches(withText("You shall not pass!")));
+    }
+
+
+
+    /**
+     * This test method inputs a password missing an lower letter and checks
+     * if the correct error message "You shall not pass!" is displayed.
+     */
+    @Test
+    public void testFindMissingLowerCase() {
+
+
+        ViewInteraction appCompatEditText = onView(withId(R.id.editText));
+
+        appCompatEditText.perform(replaceText("PPP12#$"));
+
+//        find the button with id R.id.button.
+        ViewInteraction materialButton = onView(
+                withId(R.id.button));
+//        perform clicking that button
+        materialButton.perform(click());
+//        find the view with id R.id.textView
+        ViewInteraction textView = onView(withId(R.id.textView));
+//        check that its text matches "You shall not pass!"
+        textView.check(matches(withText("You shall not pass!")));
+    }
+
+    /**
+     * This test method inputs a password missing an Special Character and checks
+     * if the correct error message "You shall not pass!" is displayed.
+     */
+    @Test
+    public void testFindMissingSpecialCharacter() {
+
+
+        ViewInteraction appCompatEditText = onView(withId(R.id.editText));
+
+        appCompatEditText.perform(replaceText("PPPabc12"));
+
+//        find the button with id R.id.button.
+        ViewInteraction materialButton = onView(
+                withId(R.id.button));
+//        perform clicking that button
+        materialButton.perform(click());
+//        find the view with id R.id.textView
+        ViewInteraction textView = onView(withId(R.id.textView));
+//        check that its text matches "You shall not pass!"
+        textView.check(matches(withText("You shall not pass!")));
+    }
+
+    /**
+     * This test method inputs a password missing an uppercase letter and checks
+     * if the correct error message "You shall not pass!" is displayed.
+     */
+    @Test
+    public void testFindMissingUpperCase(){
+
+
+        ViewInteraction appCompatEditText = onView(withId(R.id.editText));
+
+        appCompatEditText.perform(replaceText("password123#$"));
+
+//        find the button with id R.id.button.
+        ViewInteraction materialButton = onView(
+                withId(R.id.button));
+//        perform clicking that button
+        materialButton.perform(click());
+//        find the view with id R.id.textView
+        ViewInteraction textView = onView(withId(R.id.textView));
+//        check that its text matches "You shall not pass!"
+        textView.check(matches(withText("You shall not pass!")));
+
+    }
+
+    /**
+     * This test method inputs a password is complex enough and checks
+     * if the correct error message "You shall not pass!" is displayed.
+     */
+    @Test
+    public void testPasswordComplexEnough() {
+
+
+        ViewInteraction appCompatEditText = onView(withId(R.id.editText));
+
+        appCompatEditText.perform(replaceText("PPPaabc12$#"));
+
+//        find the button with id R.id.button.
+        ViewInteraction materialButton = onView(
+                withId(R.id.button));
+//        perform clicking that button
+        materialButton.perform(click());
+//        find the view with id R.id.textView
+        ViewInteraction textView = onView(withId(R.id.textView));
+//        check that its text matches "You shall not pass!"
+        textView.check(matches(withText("Your password meets the requirements")));
+    }
+}
