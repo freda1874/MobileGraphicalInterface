ed056a501f618f35fbc6dbe1cd76b6cc20bec3c5
Freda Luo <freda.luo@gmail.com>
2023-11-07 11:17:14 -0500

    gradle sync
    Signed-off-by: Freda Luo <freda.luo@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,12 +49,13 @@ android {
 }
 
 dependencies {
-
+    val roomVersion = "2.6.0"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    annotationProcessor("androidx.room:room-compiler:$roomVersion")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-
-    implementation ("androidx.recyclerview:recyclerview:1.+")
+    implementation ("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
