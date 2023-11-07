7acc1faad7c85dbd05cf56d914a23e8f52dbc260
Freda Luo <freda.luo@gmail.com>
2023-11-07 11:25:16 -0500

    update ChatMessage
    Signed-off-by: Freda Luo <freda.luo@gmail.com>

--- app/src/main/java/algonquin/cst2335/ChatMessage.java
+++ app/src/main/java/algonquin/cst2335/ChatMessage.java
@@ -1,12 +1,36 @@
-package algonquin.cst2335;public class ChatMessage {
-    String message;
-    String timeSent;
-    boolean isSentButton;
+package algonquin.cst2335;
 
-    public ChatMessage(String message, String timeSent, boolean isSentButton) {
-        this.message = message;
-        this.timeSent = timeSent;
-        this.isSentButton = isSentButton;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity //mapping variables to columns
+public class ChatMessage {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    public long Id;
+    @ColumnInfo(name = "message")
+    protected String message;
+
+
+
+    @ColumnInfo(name = "timeSent")
+    protected String timeSent;
+
+    @ColumnInfo(name = "sentOrReceive")
+    protected boolean isSentButton;
+
+
+
+    public ChatMessage(String m, String tm, boolean sr) {
+        message = m;
+        timeSent = tm;
+        isSentButton = sr;
     }
 
     public String getMessage() {
@@ -21,3 +45,4 @@ package algonquin.cst2335;public class ChatMessage {
         return isSentButton;
     }
 }
+
