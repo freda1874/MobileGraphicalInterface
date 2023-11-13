37ef066b0ca391a183819d2e7515fe338f99eb21
Freda Luo <freda.luo@gmail.com>
2023-11-13 11:42:04 -0500

    week 7?
    Signed-off-by: Freda Luo <freda.luo@gmail.com>

--- app/src/main/java/algonquin/cst2335/MessageDatabase.java
+++ app/src/main/java/algonquin/cst2335/MessageDatabase.java
@@ -3,7 +3,7 @@ package algonquin.cst2335;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {ChatMessage.class}, version = 1)
+@Database(entities = {ChatMessage.class}, version = 2)
 public abstract class MessageDatabase extends RoomDatabase {
 
     //only 1function: return the DAO object
