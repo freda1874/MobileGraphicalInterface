d2c8567c4b196990c6824bc274252f7339e834e6
Freda Luo <freda.luo@gmail.com>
2023-11-23 01:48:02 -0500

    get API working
    Signed-off-by: Freda Luo <freda.luo@gmail.com>

--- app/src/main/java/algonquin/cst2335/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/MainActivity.java
@@ -3,9 +3,11 @@ package algonquin.cst2335;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DownloadManager;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -22,6 +24,9 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
@@ -62,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
                                         "UTF-8");
 
 
-                        String stringURL = "https://api.openweathermap.org/data/2" +
+                        String stringURL = "http://api.openweathermap.org/data/2" +
                                 ".5/weather?q=" + cityName +
                                 "&appid=7e943c97096a9784391a981c4d878b22&units=metric";
 
@@ -76,10 +81,37 @@ public class MainActivity extends AppCompatActivity {
                                 String description = position0.getString("description");
                                 String iconName = position0.getString("icon");
                                 JSONObject mainObject = response.getJSONObject("main");
-                                double current = mainObject.getDouble("temp");
+                                JSONObject weatherOBJ = response.getJSONArray(
+                                        "weather").getJSONObject(0);
+
+                                runOnUiThread( (  )  -> {
+
+
+                                    try {
+                                      Double  current = mainObject.getDouble(
+                                              "temp");
+
+                                    binding.temp.setText("The current temperature  is: "+current);
+                                binding.temp.setVisibility(View.VISIBLE);
                                 double min = mainObject.getDouble("temp_min");
+                                binding.minTemp.setText("The min temperature " +
+                                        "is: "+ min);
+                                binding.minTemp.setVisibility(View.VISIBLE);
                                 double max = mainObject.getDouble("temp_max");
+                                binding.maxTemp.setText("The max temperature " +
+                                        "is: "+max);
+                                binding.maxTemp.setVisibility(View.VISIBLE);
                                 int humidity = mainObject.getInt("humidity");
+                                binding.humidity.setText("The current humidity " +
+                                        "is: "+humidity);
+                                binding.humidity.setVisibility(View.VISIBLE);
+
+                                String descript = weatherOBJ.getString(
+                                        "description");
+                                binding.description.setText(descript);
+                                binding.description.setVisibility(View.VISIBLE);} catch (JSONException e) {
+                                        throw new RuntimeException(e);
+                                    }});
 
 
 //second query for the image
@@ -89,13 +121,27 @@ public class MainActivity extends AppCompatActivity {
                                                 new Response.Listener<Bitmap>() {
                                                     public void onResponse(Bitmap bitmap) {
                                                         binding.weatherImage.setImageBitmap(bitmap);
-                                                        Log.d("MainActivity", "Image size: " + bitmap.getWidth() + "x" + bitmap.getHeight());
+                                                        binding.weatherImage.setVisibility(View.VISIBLE);
+//                                                        save the icon to the device
+
+                                                        FileOutputStream fOut = null;
+                                                        try {
+                                                            fOut = openFileOutput( iconName + ".png", Context.MODE_PRIVATE);
+
+                                                            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+                                                            fOut.flush();
+                                                            fOut.close();
+                                                        }   catch (
+                                                                IOException e) {
+                                                            throw new RuntimeException(e);
+                                                        }
                                                     }
                                                 }, 1024, 1024,
                                                 ImageView.ScaleType.FIT_CENTER, null, (error) -> {
                                             Log.e("MainActivity", "Image request error: " + error.getMessage());
                                         });
                                 queue.add(imgReq);
+
 
 
                             } catch (JSONException e) {
